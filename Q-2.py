f0ae37f7716bc0240eee6fcc96354477f1a1fb0e
Students <student@mefgi.com>
2026-08-17 09:59:29 +0530

    uplaod unit 1

---
+++ Q-2.py
@@ -0,0 +1,18 @@
+name ="devendra"
+age = 21
+height = 6.1
+student = True
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(student))
+
+
+a=10
+b=float(a)
+print(a,"->",b)
+
+c=10.99
+d=int(c)
+print(c,"->",d)
